e7a1466839608d162aae0b3b121cad73e5df4084
Pxndjj <Pattanachai_n@cmu.ac.th>
2024-06-06 01:35:37 +0700

    update select-role restaurant working

--- app/restaurant/[id]/layout.jsx
+++ app/restaurant/[id]/layout.jsx
@@ -1,28 +1,28 @@
 import RestaurantMenu from "@/components/restaurant/RestaurantMenu";
 import "../../style/admin.css";
-// export const metadata = {
-//   title: "่joyfulwait",
-//   description: "search nearby restuarant",
-// };
-// const fetchDataLayout = async (refID) => {
-//   try {
-//     const respons = await fetch(`${process.env.NEXT_PUBLIC_BACKEND}/restaurant/getdatapagebyrefid/${refID}`, {
-//       cache: 'no-store',
-//       method: 'GET',
-//       headers: {
-//         'Content-Type': 'application/json',
-//         'Accept': 'application/json'
-//       },
-//     });
-//     const dataJson = await respons.json();
-//     return dataJson;
-//   } catch (error) {
-//     console.error('Error fetching data:', error);
-//   }
-// };
+export const metadata = {
+  title: "่joyfulwait",
+  description: "search nearby restuarant",
+};
+const fetchDataLayout = async (refID) => {
+  try {
+    const respons = await fetch(`${process.env.NEXT_PUBLIC_BACKEND}/restaurant/getdatapagebyrefid/${refID}`, {
+      cache: 'no-store',
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+    });
+    const dataJson = await respons.json();
+    return dataJson;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
 
 export default async function RootLayout({ children,params}) {
-  // const dataLayout = await fetchDataLayout(params.id);
+  const dataLayout = await fetchDataLayout(params.id);
   return (
     <div className="container-admin">
         {/* Sidebar Section */}
--- app/role/page.jsx
+++ app/role/page.jsx
@@ -1,40 +1,84 @@
 "use client";
-import { Button } from "@nextui-org/react";
-import React, { useState } from 'react';
-import { useSession } from "next-auth/react";
+import { Button} from "@nextui-org/react";
+import React, {useState} from 'react';
+import { useSession } from "next-auth/react"
 import RoleSelect from '@/components/roleselect/RoleSelect';
 
+
+const api = {
+  updateRole:async(up)=>{
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND}/users/updaterole`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ data: up }),
+  });
+  const checkUser = await res.json();
+  return checkUser
+  }
+}
+
 const Role = () => {
-  const { data: session} = useSession();
-  const [role, setRole] = useState('user');
+    const { data: session,status,update} = useSession();
+    const [role, setRole] = useState('user');
 
-  const handleOnSubmit = async (e) => {
-    e
-  };
+    const handleOnSubmit = async (e) => {
+      e.preventDefault();
+      try {
+        let uptRole = {"id":session.user.id,"role":role};
+          const res = await api.updateRole(uptRole);
+          if (res) {
+            await update({role:res.userRole});
+            //ไม่ใช้ route เพื่อให้วิ่งเข้า server 
+            switch (res.userRole){
+              case "admin":
+                window.location =`${process.env.NEXT_PUBLIC_BASE_URL}/admin`;
+                break;
+              case "restaurant":
+                window.location =`${process.env.NEXT_PUBLIC_BASE_URL}/restaurant/${res._id}`;
+              break;
+              default:
+                window.location =`${process.env.NEXT_PUBLIC_BASE_URL}/user`;
+            }
 
-  return (
-    <main className="md:w-[92rem] md:px-6">
+          } 
+      } catch (error) {
+          console.log(error);
+      }
+
+    };
+
+    if(status=="loading" && session?.user?.role==''){
+        return (
+            <div>Load...</div>
+        )
+    }
+    if(session?.user?.role=='') {
+      return (
+        <main className="md:w-[92rem] md:px-6">
       <div className="md:flex m-auto h-100vh">
-        <div className="grid items-center grid-cols-1 justify-center w-full md:w-2/3 m-auto text-center h-30vh">
-          <div className="w-2/4 m-auto">
-            <p className="text-3xl">Joyfulwait</p>
-            <div><h2>Hello, {session?.user?.name} We want you to specify the type of use.</h2></div>
-          </div>
-        </div>
+      <div className="grid items-center grid-cols-1 justify-center w-full md:w-2/3 m-auto text-center h-30vh">
+                <div className="w-2/4 m-auto">
+                    <p className="text-3xl">Joyfulwait </p>
+                    <div><h2>Hello, {session.user.name} We want you to clearly specify the type of user.</h2></div>                                               
+                </div>
+      </div>
         <div className="grid items-center grid-cols-1 justify-center w-full md:w-1/3 px-2">
-          <div className="w-full m-auto card-default">
-            <form className="py-8 grid items-center grid-cols-1 justify-center" onSubmit={handleOnSubmit}>
-              <p className="text-2xl my-4 text-center">Type of user</p>
-              <RoleSelect role={role} setRole={setRole} />
-              <div className="my-3">
-                <Button className="w-full" type="submit">Next</Button>
-              </div>
+        <div className="w-full m-auto card-default">
+        <form className="py-8 grid items-center grid-cols-1 justify-center" onSubmit={handleOnSubmit}>
+        <p className="text-2xl my-4 text-center">type of user </p>
+          <RoleSelect role={role} setRole={setRole}/>
+            <div className="my-3">
+              <Button className="w-full" type="submit">GET START</Button>
+            </div>
             </form>
-          </div>
+            </div>
         </div>
       </div>
     </main>
-  );
+      );
+    }
 };
 
 export default Role;
