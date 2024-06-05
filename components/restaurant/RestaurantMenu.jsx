3cff72277e64f111825a64b065b3fe77317f004f
Fame <taninwat_p@cmu.ac.th>
2024-06-06 00:02:50 +0700

    Add restaurant page

--- components/restaurant/RestaurantMenu.jsx
+++ components/restaurant/RestaurantMenu.jsx
@@ -27,24 +27,9 @@ const RestaurantMenu = () => {
         <a className={pName === 'queue' ? 'active' : ''} href={`${bUrl}/queue`}>
           <span className="material-symbols-outlined">thumbs_up_down</span><h3>Queue</h3>
         </a>
-        {/* <a className={pName === 'promotion' ? 'active' : ''} href={`${bUrl}/promotion`}>
-          <span className="material-symbols-outlined">verified</span><h3>Promotion</h3>
-        </a>
-        <a className={pName === 'promote' ? 'active' : ''} href={`${bUrl}/promotion`}>
-        <span className="material-symbols-outlined">trophy</span><h3>Promote Brand</h3>
-        </a>  
-        <a className={pName === 'recommended' ? 'active' : ''} href={`${bUrl}/recommended`}>
-        <span className="material-symbols-outlined">thumb_up</span><h3>Recommended</h3>
-        </a>                           */}
         <a className={pName === 'setup' ? 'active' : ''} href={`${bUrl}/setup`}>
           <span className="material-symbols-outlined">manufacturing</span><h3>Setting Page</h3>
         </a>
-        {/* <a className={pName === 'photos' ? 'active' : ''} href={`${bUrl}/photos`}>
-          <span className="material-symbols-outlined">gallery_thumbnail</span><h3>Gallery</h3>
-        </a>
-        <a className={pName === 'bill' ? 'active' : ''} href={`${bUrl}/bill`}>
-          <span className="material-symbols-outlined">toggle_on</span><h3>bill</h3>
-        </a> */}
       </div>
     </aside>
   );
