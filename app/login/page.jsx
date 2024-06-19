4a7fc536649c166fbdef43fb2545a2bd884671ea
Pxndjj <Pattanachai_n@cmu.ac.th>
2024-06-20 00:17:45 +0700

    update login and register page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -4,122 +4,133 @@ import logoGoogle from "@/public/google.svg";
 import React, { useState } from "react";
 import Image from "next/image";
 import { signIn } from "next-auth/react";
-import { useRouter, useParams} from "next/navigation";
+import { useRouter, useParams } from "next/navigation";
 
 export default function Login() {
-    const router = useRouter();
-    const [credentials, setCredentials] = useState({
-        userPass: "",
-        userPhone: "",
-        userEmail: "",
-    });
-    const [messageInput, setMessageInput] = useState("");
-    const [messagelogin, setMessagelogin] = useState("");
-    const [actionLogin, setActionLogin] = useState("phone");
-    const params = useParams();
-    const handleClick = (loginType) => {
-        setActionLogin(loginType);
-    };
+  const router = useRouter();
+  const [credentials, setCredentials] = useState({
+    userPass: "",
+    userPhone: "",
+    userEmail: "",
+  });
+  const [messageInput, setMessageInput] = useState("");
+  const [messagelogin, setMessagelogin] = useState("");
+  const [actionLogin, setActionLogin] = useState("phone");
+  const params = useParams();
+  const handleClick = (loginType) => {
+    setActionLogin(loginType);
+  };
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setCredentials((prev) => ({
-            ...prev,
-            [name]: value,
-        }));
-    };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCredentials((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
-    const handleLogin = async (e) => {
-        e.preventDefault();
-        if (
-            (credentials.userEmail == "" || credentials.userPhone == "") &&
-            credentials.userPass == ""
-        ) {
-            setMessageInput("Please enter complete information!");
-        } else {
-            const resUser = await signIn("credentials", {
-                userPass: credentials.userPass,
-                userPhone: credentials.userPhone,
-                userEmail: credentials.userEmail,
-                redirect: false,
-            });
+  const handleLogin = async (e) => {
+    e.preventDefault();
 
-            console.log(resUser)
-            if (resUser.ok) {
-                setMessagelogin("Success");
-                const resModels = await fetch(`http://localhost:3000/api/auth/session`, {
-                  method: 'GET',
-                  headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                  },
-                });
-                const data = await resModels.json();
+    if (
+      (credentials.userEmail == "" || credentials.userPhone == "") &&
+      credentials.userPass == ""
+    ) {
+      setMessageInput("Please enter complete information!");
+    } else {
+      const resUser = await signIn("credentials", {
+        userPass: credentials.userPass,
+        userPhone: credentials.userPhone,
+        userEmail: credentials.userEmail,
+        redirect: false,
+      });
 
-                router.push(`${process.env.NEXT_PUBLIC_BASE_URL}/restaurant/${data.user.id}`);
-            } else {
-                setMessageInput("Please check your information is correct!");
-            }
-        }
-    };
-    return (
-      <main>
-        <div className="flex w-[50rem] m-auto">
-          <div className="w-2/4 m-auto">
-            <div className="text-3xl"><p>Joyfulwait</p></div>
-          </div>
-          <div className="w-1/5"></div>
-          <div className="w-2/5">
-            <div className="m-auto mt-5">
-              <>
-                <form onSubmit={handleLogin}>
-                  <div className="mb-2">
-                    <p className="text-xs">WELCOME BACK <spen className="text-green-500 text-sm">{messagelogin}</spen></p>
+      console.log(resUser);
+      if (resUser.ok) {
+        setMessagelogin("Success");
+        const resModels = await fetch(`http://localhost:3000/api/auth/session`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+        });
+        const data = await resModels.json();
+
+        router.push(`${process.env.NEXT_PUBLIC_BASE_URL}/restaurant/${data.user.id}`);
+      } else {
+        setMessageInput("Please check your information is correct!");
+      }
+    }
+  };
+
+  const handleRegister = () => {
+    router.push('/register');
+  }
+
+  return (
+    <main>
+      <div className="flex w-[50rem] m-auto">
+        <div className="w-2/4 m-auto">
+          <div className="text-3xl"><p>Joyfulwait</p></div>
+        </div>
+        <div className="w-1/5"></div>
+        <div className="w-2/5">
+          <div className="m-auto mt-5">
+            <>
+              <form onSubmit={handleLogin}>
+                <div className="mb-2">
+                  <p className="text-xs">WELCOME BACK <span className="text-green-500 text-sm">{messagelogin}</span></p>
+                </div>
+                <div className="mb-5">
+                  <h1 className="font-bold text-2xl">Log In to your Account </h1>
+                </div>
+                {actionLogin == "phone" ? (
+                  <div className="my-3">
+                    <Input type="text" name="userPhone" value={credentials.userPhone} onChange={handleChange} label="Phone" variant="bordered" placeholder="phone-number" className="max-w-xs m-auto" />
                   </div>
-                  <div className="mb-5">
-                    <h1 className="font-bold text-2xl">Log In to your Account </h1>
+                ) :
+                  <div className="my-3">
+                    <Input type="email" name="userEmail" value={credentials.userEmail} onChange={handleChange} label="E-mail" variant="bordered" placeholder="@gmail.com" className="max-w-xs m-auto" />
                   </div>
+                }
+                <div className="my-3">
+                  <Input type="password" name="userPass" value={credentials.userPass} onChange={handleChange} label="Password" variant="bordered" placeholder="password" className="max-w-xs m-auto" />
+                </div>
+                <div className="my-3">
+                  <p className="text-red-500 text-sm">{messageInput}</p>
+                </div>
+                <div className="my-3">
                   {actionLogin == "phone" ? (
-                    <div className="my-3">
-                      <Input type="text" name="userPhone" value={credentials.userPhone} onChange={handleChange} label="Phone" variant="bordered" placeholder="phone-number" className="max-w-xs m-auto" />
-                    </div>
-                  ) :
-                    <div className="my-3">
-                      <Input type="email" name="userEmail" value={credentials.userEmail} onChange={handleChange} label="E-mail" variant="bordered" placeholder="@gmail.com" className="max-w-xs m-auto" />
-                    </div>
-                  }
-                  <div className="my-3">
-                    <Input type="password" name="userPass" value={credentials.userPass} onChange={handleChange} label="Password" variant="bordered" placeholder="password" className="max-w-xs m-auto" />
-                  </div>
-                  <div className="my-3">
-                    <p className="text-red-500 text-sm">{messageInput}</p>
-                  </div>
-                  <div className="my-3">
-                    {actionLogin == "phone" ? (
-                      <p>Login with E-mail <span className="text-blue-500 cursor-pointer" onClick={() => handleClick("email")}>Click!!</span></p>
-                    ) : (
-                      <p>Login with Phone <span className="text-blue-500 cursor-pointer" onClick={() => handleClick("phone")}>Click!!</span></p>
-                    )}
-                  </div>
-                  <div className="my-3">
-                    <Button className="w-full" type="submit">CONTINUE</Button>
-                  </div>
-                </form>
-              </>
-              <div className="flex">
-                <div className="w-2/4"><h2 className="w-full text-center border-b leading-3 mt-3"></h2></div>
-                <div className="w-1/4 text-center"><h1>or</h1></div>
-                <div className="w-2/4"><h2 className="w-full text-center border-b leading-3 mt-3"></h2></div>
-              </div>
-              <div className="mt-4 text-center">
-                <Button className="w-full" onClick={() => { signIn("google"); }}
-                  variant="bordered" startContent={<Image src={logoGoogle} width={35} height={35} alt="123" />}>Continue with Google</Button>
-              </div>
+                    <p>Login with E-mail <span className="text-blue-500 cursor-pointer" onClick={() => handleClick("email")}>Click!!</span></p>
+                  ) : (
+                    <p>Login with Phone <span className="text-blue-500 cursor-pointer" onClick={() => handleClick("phone")}>Click!!</span></p>
+                  )}
+                </div>
+                <div className="my-3">
+                  <Button className="w-full" type="submit" style={{ backgroundColor: "#1E90FF", color: "#fff" }}>CONTINUE</Button>
+                </div>
+              </form>
+            </>
+            <div className="flex">
+              <div className="w-2/4"><h2 className="w-full text-center border-b leading-3 mt-3"></h2></div>
+              <div className="w-1/4 text-center"><h1>or</h1></div>
+              <div className="w-2/4"><h2 className="w-full text-center border-b leading-3 mt-3"></h2></div>
+            </div>
+            <div className="mt-4 text-center">
+              <Button className="w-full" onClick={() => { signIn("google"); }}
+                variant="bordered" startContent={<Image src={logoGoogle} width={35} height={35} alt="123" />}>Continue with Google</Button>
+            </div>
+            <div className="mt-4 text-center">
+              <p className="w-full" style={{transition: "color 0.3s" }}>
+                No account? <span className="cursor-pointer" onClick={handleRegister} style={{ color: "#1E90FF", transition: "color 0.3s" }}
+                  onMouseEnter={(e) => e.target.style.color = "#104E8B"}
+                  onMouseLeave={(e) => e.target.style.color = "#1E90FF"}>Register</span>
+              </p>
             </div>
           </div>
         </div>
-      </main>
-    );
-
-    
+      </div>
+    </main>
+  );
 }
--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,55 +1,72 @@
 "use client";
-import { Button, Input } from "@nextui-org/react";
+import { Button, Card, Input } from "@nextui-org/react";
 import imgeGoogle from "@/public/google.svg";
 import React, { useState } from "react";
 import Image from "next/image";
-import { useSession, signIn, signOut } from "next-auth/react"
-import { useParams, useRouter } from 'next/navigation';
-import userImage from "@/public/user.png"
+import { useSession, signIn, signOut } from "next-auth/react";
+import { useParams, useRouter } from "next/navigation";
+import userImage from "@/public/user.png";
 
 export default function Register() {
-
     const { type } = useParams();
     const { data: session } = useSession();
-    const [credentials, setCredentials] = useState({ userId: '', userName: '', userPass: '', userPhone: '', userEmail: '', userRole: '', userRegisBy: '', userImage: 'default',userStatus:'W' });
+    const router = useRouter();
+    const [credentials, setCredentials] = useState({
+        userId: "",
+        userName: "",
+        userPass: "",
+        userPhone: "",
+        userEmail: "",
+        userRole: "",
+        userRegisBy: "",
+        userImage: "default",
+        userStatus: "W",
+    });
     const [error, setError] = useState("");
     const [registerError, setRegisterError] = useState("");
 
     const api = {
-        checkUserRegister:async(objCredentials)=>{
+        checkUserRegister: async (objCredentials) => {
             const apiUrl = `${process.env.NEXT_PUBLIC_BACKEND}/users/checkuserregister?userEmail=${objCredentials.userEmail}&userPhone=${objCredentials.userPhone}`;
             const response = await fetch(apiUrl);
             const result = await response.json();
-            return result
+            return result;
         },
-        register:async(objCredentials)=>{
-            const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND}/users/register`, {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(objCredentials)
-            });
-            return res;            
-        }
-    }
+        register: async (objCredentials) => {
+            const res = await fetch(
+                `${process.env.NEXT_PUBLIC_BACKEND}/users/register`,
+                {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify(objCredentials),
+                }
+            );
+            return res;
+        },
+    };
 
-    
     //state emit update
     const handleChange = (e) => {
         const { name, value } = e.target;
         setCredentials((prev) => ({
             ...prev,
-            [name]: value
+            [name]: value,
         }));
     };
     //state event
     const handleRegister = async (e) => {
         e.preventDefault();
-        if (credentials.userEmail == "" && credentials.userName == "" && credentials.userPass == "" && credentials.userPhone == "") {
+        if (
+            credentials.userEmail == "" &&
+            credentials.userName == "" &&
+            credentials.userPass == "" &&
+            credentials.userPhone == ""
+        ) {
             setError("Please fill out the information completely!!!");
             return;
         }
-         try {
-             const checkUser = await api.checkUserRegister(credentials);
+        try {
+            const checkUser = await api.checkUserRegister(credentials);
             if (checkUser && checkUser.userPhone == credentials.userPhone) {
                 setRegisterError("This phone has already been used.");
                 return;
@@ -60,69 +77,97 @@ export default function Register() {
             }
             const res = await api.register(credentials);
             if (res.ok) {
-                await signIn('credentials', credentials);
+                await signIn("credentials", credentials);
             } else {
                 alert("resgiter unsuccessful");
             }
         } catch (error) {
             console.log(error);
         }
-
     };
 
+    const handleLogin = () => {
+        router.push('/login');
+    }
 
     if (session) {
         return (
             <div>
-                <Image src={session.userImage == "default" ? userImage : session.userImage} width={50} height={100} alt="123" />
+                <Image
+                    src={
+                        session.userImage == "default"
+                            ? userImage
+                            : session.userImage
+                    }
+                    width={50}
+                    height={100}
+                />
                 <p>Welcome {session.userName}. Signed In As</p>
                 <p>{session.userEmail}</p>
                 <button onClick={() => signOut()}>Sign out </button>
             </div>
-        )
+        );
     }
+
+    const [isVisible, setIsVisible] = React.useState(false);
+    const toggleVisibility = () => setIsVisible(!isVisible);
+
     return (
-        <main className="md:w-[80rem] md:px-6">
-            <div className="md:flex m-auto h-100vh">
-                <div className="grid items-center grid-cols-1 justify-center w-full md:w-3/4 m-auto text-center h-30vh">
-                    <div>
-                        <p className="text-4xl">Joyfulwait </p>
-                        <p className="mt-2">
-                        <span className="ml-2 "><span className="text-hiligh">No more queue line</span></span>
-                        </p>                       
+        <div className="bg-image-default h-screen flex justify-center items-end">
+            <div className="flex gap-6 w-[70%] h-[85%] text-white">
+                {/* left */}
+                <div id="left" className="flex-1">
+                    <div className="flex flex-col justify-between h-[60%] ">
+                        <div className="text-4xl font-extrabold mt-20">
+                            JoyfulWait
+                        </div>
+                        <div className="flex flex-col gap-6">
+                            <div className="text-3xl font-semibold">
+                                No more queue line
+                            </div>
+                            <div className="text-gray-200 font-extralight w-[80%]">
+                                Quickly and conveniently search and book
+                                restaurant tables online. With just one click,
+                                you can check table availability.
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="grid items-center grid-cols-1 justify-center w-full md:w-1/4 px-2">
-                    <div className="w-full m-auto card-default">
-                            <form onSubmit={handleRegister}>
-                                <div className="mb-2">
-                                    <p className="text-xs">LET'S GET YOU STARTED</p>
-                                </div>
-                                <div className="mb-5">
-                                    <h1 className="font-bold text-2xl">Create an Account </h1>
-                                </div>
-                                <div className="my-3">
-                                    <Input type="text" name="userName" value={credentials.userName} onChange={handleChange} label="User Name" variant="bordered" placeholder="name" className="max-w-xs m-auto" />
-                                </div>
-                                <div className="my-3">
-                                    <Input type="text" name="userPhone" value={credentials.userPhone} onChange={handleChange} label="Phone" variant="bordered" placeholder="phone-number" className="max-w-xs m-auto" />
-                                </div>
-                                <div className="my-3">
-                                    <Input type="email" name="userEmail" value={credentials.userEmail} onChange={handleChange} label="E-mail" variant="bordered" placeholder="@gmail.com" className="max-w-xs m-auto" />
-                                </div>
-                                <div className="my-3">
-                                    <Input type="password" name="userPass" value={credentials.userPass} onChange={handleChange} label="Password" variant="bordered" placeholder="password" className="max-w-xs m-auto" />
-                                </div>
-                                <div className="my-3">
-                                    <p className="text-red-500 w-full text-sm">{error}</p>
-                                </div>
-                                <div className="my-3">
-                                    <p className="text-red-500 w-full text-sm">{registerError}</p>
-                                </div>
-                                <div className="my-3">
-                                    <Button className="w-full" type="submit">GET STARTED</Button>
-                                </div>
-                            </form>
+                {/* right */}
+
+                <div id="right" className="flex-1">
+
+                    <Card className="h-full w-full rounded-b-none p-8 overflow-auto">
+
+                        <form onSubmit={handleRegister}>
+                            <div className="mb-2">
+                                <p className="text-xs">LET'S GET YOU STARTED</p>
+                            </div>
+                            <div className="mb-5">
+                                <h1 className="font-bold text-2xl">Create an Account </h1>
+                            </div>
+                            <div className="my-3">
+                                <Input type="text" name="userName" value={credentials.userName} onChange={handleChange} label="User Name" variant="bordered" placeholder="name" className="m-auto" />
+                            </div>
+                            <div className="my-3">
+                                <Input type="text" name="userPhone" value={credentials.userPhone} onChange={handleChange} label="Phone" variant="bordered" placeholder="phone-number" className="m-auto" />
+                            </div>
+                            <div className="my-3">
+                                <Input type="email" name="userEmail" value={credentials.userEmail} onChange={handleChange} label="E-mail" variant="bordered" placeholder="@gmail.com" className="m-auto" />
+                            </div>
+                            <div className="my-3">
+                                <Input type="password" name="userPass" value={credentials.userPass} onChange={handleChange} label="Password" variant="bordered" placeholder="password" className="m-auto" />
+                            </div>
+                            <div className="my-3">
+                                <p className="text-red-500 w-full text-sm">{error}</p>
+                            </div>
+                            <div className="my-3">
+                                <p className="text-red-500 w-full text-sm">{registerError}</p>
+                            </div>
+                            <div className="my-3">
+                                <Button className="w-full" type="submit">GET STARTED</Button>
+                            </div>
+                        </form>
                         <div className="flex">
                             <div className="w-2/4"><h2 className="w-full text-center border-b leading-3 mt-3"></h2></div>
                             <div className="w-1/4 text-center"><h1>or</h1></div>
@@ -131,9 +176,19 @@ export default function Register() {
                         <div className="mt-4 text-center">
                             <Button className="w-full" onClick={() => { signIn("google"); }} variant="bordered" startContent={<Image src={imgeGoogle} width={35} height={35} alt="123" />}>Continue with Google</Button>
                         </div>
-                    </div>
+                        <div className="mt-4 text-center">
+                            <p className="w-full" style={{ transition: "color 0.3s" }}>
+                            Already have an account? <span className="cursor-pointer" onClick={handleLogin} style={{ color: "#1E90FF", transition: "color 0.3s" }}
+                                    onMouseEnter={(e) => e.target.style.color = "#104E8B"}
+                                    onMouseLeave={(e) => e.target.style.color = "#1E90FF"}>Login</span>
+                            </p>
+                        </div>
+
+                    </Card>
+
                 </div>
+
             </div>
-        </main>
+        </div>
     );
 }
