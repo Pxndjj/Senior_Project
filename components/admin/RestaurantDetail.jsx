bc28888d64cd2a1c2493c95319e31e672f9e0a78
Fame <taninwat_p@cmu.ac.th>
2024-08-23 00:59:44 +0700

    Edit size of image and edit some word

--- components/admin/RestaurantDetail.jsx
+++ components/admin/RestaurantDetail.jsx
@@ -18,7 +18,6 @@ const backgroundStyle = (path) => {
 
 const RestaurantDetails = ({ data, onRefresh }) => {
 
-    // ตรวจสอบว่า data.recommendedMenu เป็นอาร์เรย์หรือไม่
     const recommendedMenu = Array.isArray(data.recommendedMenu) ? data.recommendedMenu : [];
 
     const [message, setMessage] = useState("");
@@ -78,38 +77,39 @@ const RestaurantDetails = ({ data, onRefresh }) => {
             <div className="container mx-auto p-6 lg:p-12">
                 <div className="bg-white shadow-md rounded-xl border border-gray-300 overflow-hidden flex flex-col lg:flex-row">
                     <div className="relative lg:w-2/3 flex-shrink-0">
-                        <img
-                            src={data.image}
-                            alt="Restaurant Logo"
-                            className="w-full h-full object-cover rounded-t-xl lg:rounded-tr-none lg:rounded-l-xl"
-                        />
+                    <img
+                        src={data.image}
+                        alt="Restaurant Logo"
+                        className="w-full h-full object-cover rounded-t-xl lg:rounded-tr-none lg:rounded-l-xl"
+                        style={{ maxHeight: '300%', minHeight: '300px', aspectRatio: '16/9' }}
+                    />
                     </div>
 
                     <div className="p-6 lg:w-1/3 flex flex-col">
                         <div className="text-center lg:text-left mb-6">
                             <h2 className="text-3xl font-bold text-gray-900">{data.name}</h2>
                             <p className="text-sm text-gray-600 mt-1">{data.address}</p>
-                            <p className="text-sm text-gray-600">โทร: {data.phone}</p>
+                            <p className="text-sm text-gray-600">phone: {data.phone}</p>
                         </div>
                         <div className="mb-6">
-                            <h3 className="text-2xl font-semibold mb-3 text-gray-900">เวลาเปิดทำการ</h3>
+                            <h3 className="text-2xl font-semibold mb-3 text-gray-900">Opening hour</h3>
                             <ul className="space-y-2 text-gray-700">
-                                {Object.keys(data.openingHours || {}).filter(o => o !== "_id").map((day) => (
+                                {Object.keys(data.openingHours).filter(o => o != "_id").map((day) => (
                                     <li key={day} className="flex justify-between text-sm">
                                         <span className="capitalize font-medium">{day}:</span>
-                                        <span>{data.openingHours[day]?.start} - {data.openingHours[day]?.to}</span>
+                                        <span>{data.openingHours[day].start} - {data.openingHours[day].to}</span>
                                     </li>
                                 ))}
                             </ul>
                         </div>
                         <div className="mb-6">
-                            <h3 className="text-2xl font-semibold mb-3 text-gray-900">หมายเหตุ</h3>
+                            <h3 className="text-2xl font-semibold mb-3 text-gray-900">Note</h3>
                             <p className="text-gray-700">{data.notes}</p>
                         </div>
                         <div>
-                            <h3 className="text-2xl font-semibold mb-3 text-gray-900">เงื่อนไข</h3>
+                            <h3 className="text-2xl font-semibold mb-3 text-gray-900">Conditions</h3>
                             <ul className="list-disc list-inside space-y-2 text-gray-700">
-                                {data.conditions?.map((condition, index) => (
+                                {data.conditions.map((condition, index) => (
                                     <li key={index} className="text-sm">{condition}</li>
                                 ))}
                             </ul>
